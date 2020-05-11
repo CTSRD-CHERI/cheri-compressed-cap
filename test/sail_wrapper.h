8c5896a38e4621343eb2bfd52101af373fd55333
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-11 09:31:54 +0100

    Add a wrapper for sail-generated C code
    This will be useful for testing and fuzzing. It should also make it easier
    to generate test cases for the future CHERI-64 implementation.

---
+++ test/sail_wrapper.h
@@ -0,0 +1,13 @@
+#include "../cheri_compressed_cap.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+void sail_decode_mem128(uint64_t pesbt, uint64_t cursor, bool tag, cap_register_t* cdp);
+void sail_decode_raw128(uint64_t pesbt, uint64_t cursor, bool tag, cap_register_t* cdp);
+uint64_t sail_compress_128_raw(const cap_register_t* csp);
+uint64_t sail_compress_128_for_mem(const cap_register_t* csp);
+
+uint64_t sail_null_pesbt(void);
+#ifdef __cplusplus
+}
+#endif
