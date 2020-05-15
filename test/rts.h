766632c8e521d41690f5a073841c60d520130f8d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-15 14:55:39 +0100

    Avoid double initialization of sail library state

--- test/rts.h
+++ test/rts.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <stdlib.h>
+
 /* ***** Setup and cleanup functions for RTS ***** */
 static int process_arguments(int argc, char** argv) { abort(); }
-static void setup_rts(void) { setup_library(); }
-static void cleanup_rts(void) { cleanup_library(); }
+void setup_rts(void);
+void cleanup_rts(void);
 
 // this is needed for some reason:
 static bool have_exception = false;
