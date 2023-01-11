c1b8a5a61987d63eb94be1e5f105ba251566b84e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-01-11 14:36:27 +0000

    Restrict _cc_N(setbounds_impl) validity assertions to tagged capabilities
    When called on untagged quantities these do not necessarily hold.

--- cheri_compressed_cap_common.h
+++ cheri_compressed_cap_common.h
@@ -840,13 +840,16 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
      */
     const _cc_addr_t cursor = cap->_cr_cursor;
 #ifndef CC_IS_MORELLO
-    _cc_debug_assert(((cap->_cr_top - cap->cr_base) >> _CC_ADDR_WIDTH) <= 1 && "Length must be smaller than 1 << 65");
-    _cc_debug_assert((req_top >> _CC_ADDR_WIDTH) <= 1 && "New top must be smaller than 1 << 65");
-    _cc_debug_assert(req_base >= cap->cr_base && "Cannot decrease base");
-    _cc_debug_assert(req_top <= cap->_cr_top && "Cannot increase top");
-    assert((cursor < cap->_cr_top || (cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
-           "Must be used on inbounds caps or request zero-length cap at top");
-    assert((cursor >= cap->cr_base) && "Must be used on inbounds caps");
+    if (cap->cr_tag) {
+        _cc_debug_assert(((cap->_cr_top - cap->cr_base) >> _CC_ADDR_WIDTH) <= 1 &&
+                         "Length must be smaller than 1 << 65");
+        _cc_debug_assert((req_top >> _CC_ADDR_WIDTH) <= 1 && "New top must be smaller than 1 << 65");
+        _cc_debug_assert(req_base >= cap->cr_base && "Cannot decrease base");
+        _cc_debug_assert(req_top <= cap->_cr_top && "Cannot increase top");
+        assert((cursor < cap->_cr_top || (cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
+               "Must be used on inbounds caps or request zero-length cap at top");
+        assert((cursor >= cap->cr_base) && "Must be used on inbounds caps");
+    }
 #endif
     _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 3, "expected 3 bits to be used by");  // expected 3 bits to
     _CC_STATIC_ASSERT(_CC_EXP_HIGH_WIDTH == 3, "expected 3 bits to be used by"); // expected 3 bits to
