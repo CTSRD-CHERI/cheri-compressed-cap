11c1999f5e3bc066c74c103cf5ecb1020ce03b6f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-05-11 11:29:40 -0700

    Add helper functions to simplify fast rep tests
    This avoids lots of duplicate code in these similar tests.

--- test/simple_test_128m.cpp
+++ test/simple_test_128m.cpp
@@ -6,84 +6,35 @@
 
 TEST_CASE("Fast representability check sign extended", "[fuzz]") {
     // Check that the fast representability check properly accounts for Morello sign extension on the address.
-    TestAPICC::cap_t cap;
-    TestAPICC::decompress_raw(0xa1a04fffffdff00, 0x01a4fe02410001ff, false, &cap);
-    CHECK(cap.base() == 0xfeff000000000000);
-    CHECK(cap.top() == 0xff3ff80000000000);
-    TestAPICC::addr_t new_addr = 0;
-    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(&cap, new_addr);
-    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
-    CHECK(sail_fast_rep == cc_fast_rep);
-    CHECK(!cc_fast_rep);
-    // It should also not be representable if we do the full check.
-    CHECK(!_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
+    checkRepCheckFails(/*pesbt=*/0xa1a04fffffdff00, /*addr=*/0x01a4fe02410001ff,
+                       /*expected_base=*/0xfeff000000000000, /*expected_top=*/0xff3ff80000000000,
+                       /*new_addr=*/0);
 }
 
 TEST_CASE("Fast representability check zero length", "[fuzz]") {
     // Check that the fast representability check does treat zero length caps as always representable
     // This is not true if the cursor change results in a sign change on the resulting bounds.
-    TestAPICC::cap_t cap;
-    TestAPICC::decompress_raw(0xaff000000000000, 0x0080000000800000, false, &cap);
-    CHECK(cap.base() == 0xff80000000800007);
-    CHECK(cap.top() == 0xff80000000800007);
-    TestAPICC::addr_t new_addr = 0;
-    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(&cap, new_addr);
-    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
-    CHECK(sail_fast_rep == cc_fast_rep);
-    CHECK(!cc_fast_rep);
-    // It should also not be representable if we do the full check.
-    CHECK(!_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
+    checkRepCheckFails(/*pesbt=*/0xaff000000000000, /*addr=*/0x0080000000800000,
+                       /*expected_base=*/0xff80000000800007, /*expected_top=*/0xff80000000800007,
+                       /*new_addr=*/0);
 }
 
 TEST_CASE("Fast representability zero length no sign change", "[fuzz]") {
     // Check that the fast representability check does treat zero length caps as always representable
     // Even when the sign of the cursor does not change the bounds interpretation could.
-    TestAPICC::cap_t cap;
-    TestAPICC::decompress_raw(0xffff000000000000, 0x0029ffffff85ffff, false, &cap);
-    CHECK(cap.base() == 0x0029ffffff860007);
-    CHECK(cap.top() == 0x0029ffffff860007);
-    TestAPICC::addr_t new_addr = 0;
-    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(&cap, new_addr);
-    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
-    CHECK(sail_fast_rep == cc_fast_rep);
-    CHECK(!cc_fast_rep);
-    // It should also not be representable if we do the full check since the bounds interpretation changes.
-    CHECK(!_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
-    _cc_N(set_addr)(&cap, new_addr);
-    CHECK(cap.base() == 7);
-    CHECK(cap.top() == 7);
+    checkRepCheckFails(/*pesbt=*/0xffff000000000000, /*addr=*/0x0029ffffff85ffff,
+                       /*expected_base=*/0x0029ffffff860007, /*expected_top=*/0x0029ffffff860007,
+                       /*new_addr=*/0);
 }
 
 TEST_CASE("Fuzzer-detected fast representability mismatch 1", "[fuzz]") {
-    TestAPICC::cap_t cap;
-    TestAPICC::decompress_raw(0x81000100ffeeffd2, 0x0045000000000000, false, &cap);
-    CHECK(cap.base() == 0xff40000000000000);
-    CHECK(cap.top() == 0x0ffa0000000000000);
-    TestAPICC::addr_t new_addr = 0;
-    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(&cap, new_addr);
-    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
-    CHECK(sail_fast_rep == cc_fast_rep);
-    CHECK(cc_fast_rep);
-    // It should also not be representable if we do the full check since the bounds interpretation changes.
-    CHECK(_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
-    _cc_N(set_addr)(&cap, new_addr);
-    CHECK(cap.base() == 0xff40000000000000);
-    CHECK(cap.top() == 0x0ffa0000000000000);
+    checkFastRepCheckSucceeds(/*pesbt=*/0x81000100ffeeffd2, /*addr=*/0x0045000000000000,
+                              /*expected_base=*/0xff40000000000000, /*expected_top=*/0x0ffa0000000000000,
+                              /*new_addr=*/0);
 }
 
 TEST_CASE("Fast representability mismatch 2", "[fuzz]") {
-    TestAPICC::cap_t cap;
-    TestAPICC::decompress_raw(0x9a9a656665659a9a, 0x00100a9a9a9a9a9a, false, &cap);
-    CHECK(cap.base() == 0xfe6a600000000000);
-    CHECK(cap.top() == 0xff95800000000000);
-    TestAPICC::addr_t new_addr = 0;
-    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(&cap, new_addr);
-    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
-    CHECK(sail_fast_rep == cc_fast_rep);
-    CHECK(cc_fast_rep);
-    // It should also not be representable if we do the full check since the bounds interpretation changes.
-    CHECK(_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
-    _cc_N(set_addr)(&cap, new_addr);
-    CHECK(cap.base() == 0xfe6a600000000000);
-    CHECK(cap.top() == 0xff95800000000000);
+    checkFastRepCheckSucceeds(/*pesbt=*/0x9a9a656665659a9a, /*addr=*/0x00100a9a9a9a9a9a,
+                              /*expected_base=*/0xfe6a600000000000, /*expected_top=*/0xff95800000000000,
+                              /*new_addr=*/0);
 }
