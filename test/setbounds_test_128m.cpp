851c1cc82c0048c49f4571990b64bb2fd6ae7701
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-05-11 11:44:13 -0700

    Add a test that Morello flags are ignored by setbounds

--- test/setbounds_test_128m.cpp
+++ test/setbounds_test_128m.cpp
@@ -2,6 +2,17 @@
 #define TEST_CC_FORMAT_UPPER 128M
 #define TEST_CC_IS_MORELLO
 
-// TODO: Generate test cases from sail
-
 #include "setbounds_test_common.cpp"
+
+TEST_CASE("Morello setbounds with high bits", "[bounds]") {
+    // Check that Morello flags are ignored correctly.
+    const TestAPICC::cap_t cap = TestAPICC::make_max_perms_cap(0, UINT64_C(0xFF00000000000944), _CC_N(MAX_LENGTH));
+    REQUIRE(cap.address() == 0xFF00000000000944);
+    bool was_exact = false;
+    auto result = do_csetbounds<TestAPICC>(cap, &was_exact, 16);
+    CHECK(result.address() == 0xFF00000000000944);
+    CHECK(result.length() == 16);
+    CHECK(result.base() == 0x944); // Base should not include the high 8 bits
+    CHECK(result.top() == 0x954);  // Same for top
+    CHECK(was_exact);
+}
