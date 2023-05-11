eb8264ca12e858e12251de53c868bb570dc8ed3b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-05-11 11:29:14 -0700

    Remove zero-length special case from fast representability check
    This is not correct, when the capability has zero length bounds
    representation can change after updating the address.

--- test/simple_test_128m.cpp
+++ test/simple_test_128m.cpp
@@ -32,3 +32,21 @@ TEST_CASE("Fast representability check zero length", "[fuzz]") {
     // It should also not be representable if we do the full check.
     CHECK(!_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
 }
+
+TEST_CASE("Fast representability zero length no sign change", "[fuzz]") {
+    // Check that the fast representability check does not treat zero length caps as always representable.
+    // Even when the sign of the cursor does not change the bounds interpretation could.
+    TestAPICC::cap_t cap = TestAPICC::decompress_raw(0xffff000000000000, 0x0029ffffff85ffff, false);
+    CHECK(cap.base() == 0x0029ffffff860007);
+    CHECK(cap.top() == 0x0029ffffff860007);
+    TestAPICC::addr_t new_addr = 0;
+    bool sail_fast_rep = TestAPICC::sail_fast_is_representable(cap, new_addr);
+    bool cc_fast_rep = _cc_N(fast_is_representable_new_addr)(&cap, new_addr);
+    CHECK(sail_fast_rep == cc_fast_rep);
+    CHECK(!cc_fast_rep);
+    // It should also not be representable if we do the full check since the bounds interpretation changes.
+    CHECK(!_cc_N(is_representable_with_addr_impl)(&cap, new_addr, /*slow_representable_check=*/true));
+    _cc_N(set_addr)(&cap, new_addr);
+    CHECK(cap.base() == 7);
+    CHECK(cap.top() == 7);
+}
