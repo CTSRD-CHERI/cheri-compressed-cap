d6f29c50f335be471be81aa83c5fb198d597719e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-05-11 11:44:12 -0700

    Change the setbounds API to match the instructions
    Being able to pass requested top and base is somewhat error-prone as
    for Morello the masking of high bits needs to be taken into account.
    Hide this implementation detail by passing a length instead.
    This is the first step towards avoiding problems like the QEMU regression
    introduced in commit 1a3898fa066e9f7e0560ccc95731d5b7f3a7c882 which caused
    the Morello testsuite to fail.

--- test/setbounds_test_128m.cpp
+++ test/setbounds_test_128m.cpp
@@ -2,15 +2,13 @@
 #define TEST_CC_FORMAT_UPPER 128M
 #define TEST_CC_IS_MORELLO
 
-// TODO: Generate test cases from sail
-
 #include "setbounds_test_common.cpp"
 
 TEST_CASE("Morello setbounds with high bits", "[bounds]") {
     // Check that Morello flags are not changed when we set the base to exclude them.
     TestAPICC::cap_t cap = TestAPICC::make_max_perms_cap(0, UINT64_C(0xFF00000000000944), _CC_N(MAX_LENGTH));
     REQUIRE(cap.address() == 0xFF00000000000944);
-    bool was_exact = TestAPICC::setbounds(&cap, 0x944, 0x954);
+    bool was_exact = TestAPICC::setbounds(&cap, 16);
     CHECK(cap.address() == 0xFF00000000000944);
     CHECK(cap.length() == 16);
     CHECK(cap.base() == 0x944); // Base should not include the high 8 bits
