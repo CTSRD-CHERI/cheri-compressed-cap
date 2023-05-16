CTSRD-CHERI/cheri-compressed-cap

commit 1a3898fa066e9f7e0560ccc95731d5b7f3a7c882

Fix and add test for setting bounds on a non-canonical untagged capability
This was found by the fuzzer since it triggered an assertion, now it
matches the sail outputs.
When calling decompress_raw we have to use req_base and not the current
address to infer the bounds. This did not matter previously since we only
called the function on in-bounds caps, so the result was the same, but now
that we allow out-of-bounds untagged values, we have to use req_base
instead and set the address to req_base.

cheri_compressed_cap_common.h
@@ -829,8 +829,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     bool from_large = !_cc_N(cap_bounds_uses_value)(cap);
 #else
     // Morello allows setbounds to do weird things and will just result in untagged results
-    _cc_debug_assert((cap->cr_tag) && "Cannot be used on untagged capabilities");
-    _cc_debug_assert((!_cc_N(is_cap_sealed)(cap)) && "Cannot be used on sealed capabilities");
+    _cc_debug_assert(!(cap->cr_tag && _cc_N(is_cap_sealed)(cap)) && "Cannot be used on tagged sealed capabilities");
 #endif
     _cc_debug_assert(req_base <= req_top && "Cannot invert base and top");
     /*
@@ -838,7 +837,6 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
      * memory addresses to be wider than requested so it is
      * representable.
      */
-    const _cc_addr_t cursor = cap->_cr_cursor;
 #ifndef CC_IS_MORELLO
     if (cap->cr_tag) {
         _cc_debug_assert(((cap->_cr_top - cap->cr_base) >> _CC_ADDR_WIDTH) <= 1 &&
@@ -846,9 +844,10 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
         _cc_debug_assert((req_top >> _CC_ADDR_WIDTH) <= 1 && "New top must be smaller than 1 << 65");
         _cc_debug_assert(req_base >= cap->cr_base && "Cannot decrease base");
         _cc_debug_assert(req_top <= cap->_cr_top && "Cannot increase top");
-        assert((cursor < cap->_cr_top || (cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
+        assert((cap->_cr_cursor < cap->_cr_top ||
+                (cap->_cr_cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
                "Must be used on inbounds caps or request zero-length cap at top");
-        assert((cursor >= cap->cr_base) && "Must be used on inbounds caps");
+        assert((cap->_cr_cursor >= cap->cr_base) && "Must be used on inbounds caps");
     }
 #endif
     _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 3, "expected 3 bits to be used by");  // expected 3 bits to
@@ -860,7 +859,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     const _cc_addr_t pesbt = _CC_ENCODE_FIELD(0, UPERMS) | _CC_ENCODE_FIELD(0, HWPERMS) |
                              _CC_ENCODE_FIELD(_CC_N(OTYPE_UNSEALED), OTYPE) | _CC_ENCODE_FIELD(new_ebt, EBT);
     _cc_cap_t new_cap;
-    _cc_N(decompress_raw)(pesbt, cursor, cap->cr_tag, &new_cap);
+    _cc_N(decompress_raw)(pesbt, req_base, cap->cr_tag, &new_cap);
     _cc_addr_t new_base = new_cap.cr_base;
     _cc_length_t new_top = new_cap._cr_top;
 
@@ -880,6 +879,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     _cc_debug_assert(new_top >= new_base);
     _cc_debug_assert((!cap->cr_tag || _cc_N(get_reserved)(cap) == 0) &&
                      "Unknown reserved bits set in tagged capability");
+    cap->_cr_cursor = req_base;
     cap->cr_base = new_base;
     cap->_cr_top = new_top;
     cap->cr_exp = new_cap.cr_exp;

test/setbounds_test_128.cpp
@@ -61,3 +61,34 @@ TEST_CASE("Cheritest regression case", "[regression]") {
     CHECK(with_bounds.length() == 0x300000);
     CHECK(with_bounds.address() == cap.address());
 }
+
+TEST_CASE("Fuzzer assertion exact untagged", "[fuzz]") {
+    // Regression test for an assertion found by fuzzing setbounds.
+    constexpr uint64_t pesbt = UINT64_C(0x00f58dffffffffff);
+    constexpr uint64_t base = UINT64_C(0xff80000000000000);
+    constexpr uint64_t addr = UINT64_C(0xf806ffffffff3e0a);
+    constexpr TestAPICC::length_t top = base;
+    TestAPICC::cap_t decoded;
+    TestAPICC::decompress_raw(pesbt, addr, false, &decoded);
+    REQUIRE(decoded.base() == base);
+    REQUIRE(decoded.top() == top);
+    REQUIRE(decoded.address() == addr);
+    {
+        auto sail_setbounds_base_top = decoded;
+        auto cc_setbounds_base_top = decoded;
+        TestAPICC::sail_setbounds(&sail_setbounds_base_top, base, top);
+        TestAPICC::setbounds(&cc_setbounds_base_top, base, top);
+        CHECK(sail_setbounds_base_top == cc_setbounds_base_top);
+    }
+    {
+        auto sail_setbounds_addr_top = decoded;
+        auto cc_setbounds_addr_top = decoded;
+        TestAPICC::sail_setbounds(&sail_setbounds_addr_top, addr, top);
+        TestAPICC::setbounds(&cc_setbounds_addr_top, addr, top);
+        CHECK(cc_setbounds_addr_top == cc_setbounds_addr_top);
+    }
+
+    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+    decoded._cr_cursor = base;
+    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+}

test/setbounds_test_common.cpp
@@ -38,8 +38,10 @@ static inline void check_csetbounds_invariants(const typename Handler::cap_t& in
         // At least one must be different otherwise was_exact is not correct
         REQUIRE((with_bounds.top() != requested_top || with_bounds.base() != requested_base));
     }
-    REQUIRE(with_bounds.base() >= initial_cap.base());          // monotonicity broken
-    REQUIRE(with_bounds.top() <= initial_cap.top());            // monotonicity broken
+    if (with_bounds.cr_tag) {
+        REQUIRE(with_bounds.base() >= initial_cap.base()); // monotonicity broken
+        REQUIRE(with_bounds.top() <= initial_cap.top());   // monotonicity broken
+    }
     REQUIRE(Handler::is_representable_cap_exact(&with_bounds)); // result of csetbounds must be representable
 }
 
@@ -64,11 +66,13 @@ static typename Handler::cap_t do_csetbounds(const typename Handler::cap_t& init
     CHECK(with_bounds.cr_pesbt == Handler::compress_raw(&with_bounds));
     CHECK(with_bounds.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
     // Re-create the bounded capability and assert that the current pesbt values matches that one.
-    auto new_cap = Handler::make_max_perms_cap(with_bounds.base(), with_bounds.address(), with_bounds.top());
-    CHECK(new_cap == with_bounds);
-    CHECK(new_cap.cr_pesbt == with_bounds.cr_pesbt);
-    CHECK(new_cap.cr_pesbt == Handler::compress_raw(&with_bounds));
-    CHECK(new_cap.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
+    if (with_bounds.cr_tag) {
+        auto new_cap = Handler::make_max_perms_cap(with_bounds.base(), with_bounds.address(), with_bounds.top());
+        CHECK(new_cap == with_bounds);
+        CHECK(new_cap.cr_pesbt == with_bounds.cr_pesbt);
+        CHECK(new_cap.cr_pesbt == Handler::compress_raw(&with_bounds));
+        CHECK(new_cap.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
+    }
 
     if (was_exact)
         *was_exact = exact;

commit a01d0da8fb05e33cbbd5781b1f21a573a631cc87

Avoid updating the cursor inside setbounds_impl
The cursor should never change as part of this function. This change from
commit 1a3898fa066e9f7e0560ccc95731d5b7f3a7c882 broke the QEMU Morello
regression tests since we were now stripping the high bits of capabilities
in some cases

cheri_compressed_cap_common.h
@@ -817,7 +817,6 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
         _cc_debug_assert(new_top >= new_base);
         _cc_debug_assert(_cc_N(get_reserved)(cap) == 0 && "Unknown reserved bits set in tagged capability");
     }
-    cap->_cr_cursor = req_base;
     cap->cr_base = new_base;
     cap->_cr_top = new_top;
     _cc_N(update_ebt)(cap, new_ebt);