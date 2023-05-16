CTSRD-CHERI/cheri-compressed-cap

commit 1a3898fa066e9f7e0560ccc95731d5b7f3a7c882

Fix and add test for setting bounds on a non-canonical untagged capability
This was found by the fuzzer since it triggered an assertion, now it
matches the sail outputs.
When calling decompress_raw we have to use req_base and not the current
address to infer the bounds. This did not matter previously since we only
called the function on in-bounds caps, so the result was the same, but now
that we allow out-of-bounds untagged values, we have to use req_base
instead and set the address to req_base.

cheri_compressed_cap_common.h
@@ -829,8 +829,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     bool from_large = !_cc_N(cap_bounds_uses_value)(cap);
 #else
     // Morello allows setbounds to do weird things and will just result in untagged results
-    _cc_debug_assert((cap->cr_tag) && "Cannot be used on untagged capabilities");
-    _cc_debug_assert((!_cc_N(is_cap_sealed)(cap)) && "Cannot be used on sealed capabilities");
+    _cc_debug_assert(!(cap->cr_tag && _cc_N(is_cap_sealed)(cap)) && "Cannot be used on tagged sealed capabilities");
 #endif
     _cc_debug_assert(req_base <= req_top && "Cannot invert base and top");
     /*
@@ -838,7 +837,6 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
      * memory addresses to be wider than requested so it is
      * representable.
      */
-    const _cc_addr_t cursor = cap->_cr_cursor;
 #ifndef CC_IS_MORELLO
     if (cap->cr_tag) {
         _cc_debug_assert(((cap->_cr_top - cap->cr_base) >> _CC_ADDR_WIDTH) <= 1 &&
@@ -846,9 +844,10 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
         _cc_debug_assert((req_top >> _CC_ADDR_WIDTH) <= 1 && "New top must be smaller than 1 << 65");
         _cc_debug_assert(req_base >= cap->cr_base && "Cannot decrease base");
         _cc_debug_assert(req_top <= cap->_cr_top && "Cannot increase top");
-        assert((cursor < cap->_cr_top || (cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
+        assert((cap->_cr_cursor < cap->_cr_top ||
+                (cap->_cr_cursor == cap->_cr_top && req_base == cap->_cr_top && req_base == req_top)) &&
                "Must be used on inbounds caps or request zero-length cap at top");
-        assert((cursor >= cap->cr_base) && "Must be used on inbounds caps");
+        assert((cap->_cr_cursor >= cap->cr_base) && "Must be used on inbounds caps");
     }
 #endif
     _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 3, "expected 3 bits to be used by");  // expected 3 bits to
@@ -860,7 +859,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     const _cc_addr_t pesbt = _CC_ENCODE_FIELD(0, UPERMS) | _CC_ENCODE_FIELD(0, HWPERMS) |
                              _CC_ENCODE_FIELD(_CC_N(OTYPE_UNSEALED), OTYPE) | _CC_ENCODE_FIELD(new_ebt, EBT);
     _cc_cap_t new_cap;
-    _cc_N(decompress_raw)(pesbt, cursor, cap->cr_tag, &new_cap);
+    _cc_N(decompress_raw)(pesbt, req_base, cap->cr_tag, &new_cap);
     _cc_addr_t new_base = new_cap.cr_base;
     _cc_length_t new_top = new_cap._cr_top;
 
@@ -880,6 +879,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     _cc_debug_assert(new_top >= new_base);
     _cc_debug_assert((!cap->cr_tag || _cc_N(get_reserved)(cap) == 0) &&
                      "Unknown reserved bits set in tagged capability");
+    cap->_cr_cursor = req_base;
     cap->cr_base = new_base;
     cap->_cr_top = new_top;
     cap->cr_exp = new_cap.cr_exp;

test/setbounds_test_128.cpp
@@ -61,3 +61,34 @@ TEST_CASE("Cheritest regression case", "[regression]") {
     CHECK(with_bounds.length() == 0x300000);
     CHECK(with_bounds.address() == cap.address());
 }
+
+TEST_CASE("Fuzzer assertion exact untagged", "[fuzz]") {
+    // Regression test for an assertion found by fuzzing setbounds.
+    constexpr uint64_t pesbt = UINT64_C(0x00f58dffffffffff);
+    constexpr uint64_t base = UINT64_C(0xff80000000000000);
+    constexpr uint64_t addr = UINT64_C(0xf806ffffffff3e0a);
+    constexpr TestAPICC::length_t top = base;
+    TestAPICC::cap_t decoded;
+    TestAPICC::decompress_raw(pesbt, addr, false, &decoded);
+    REQUIRE(decoded.base() == base);
+    REQUIRE(decoded.top() == top);
+    REQUIRE(decoded.address() == addr);
+    {
+        auto sail_setbounds_base_top = decoded;
+        auto cc_setbounds_base_top = decoded;
+        TestAPICC::sail_setbounds(&sail_setbounds_base_top, base, top);
+        TestAPICC::setbounds(&cc_setbounds_base_top, base, top);
+        CHECK(sail_setbounds_base_top == cc_setbounds_base_top);
+    }
+    {
+        auto sail_setbounds_addr_top = decoded;
+        auto cc_setbounds_addr_top = decoded;
+        TestAPICC::sail_setbounds(&sail_setbounds_addr_top, addr, top);
+        TestAPICC::setbounds(&cc_setbounds_addr_top, addr, top);
+        CHECK(cc_setbounds_addr_top == cc_setbounds_addr_top);
+    }
+
+    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+    decoded._cr_cursor = base;
+    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+}

test/setbounds_test_common.cpp
@@ -38,8 +38,10 @@ static inline void check_csetbounds_invariants(const typename Handler::cap_t& in
         // At least one must be different otherwise was_exact is not correct
         REQUIRE((with_bounds.top() != requested_top || with_bounds.base() != requested_base));
     }
-    REQUIRE(with_bounds.base() >= initial_cap.base());          // monotonicity broken
-    REQUIRE(with_bounds.top() <= initial_cap.top());            // monotonicity broken
+    if (with_bounds.cr_tag) {
+        REQUIRE(with_bounds.base() >= initial_cap.base()); // monotonicity broken
+        REQUIRE(with_bounds.top() <= initial_cap.top());   // monotonicity broken
+    }
     REQUIRE(Handler::is_representable_cap_exact(&with_bounds)); // result of csetbounds must be representable
 }
 
@@ -64,11 +66,13 @@ static typename Handler::cap_t do_csetbounds(const typename Handler::cap_t& init
     CHECK(with_bounds.cr_pesbt == Handler::compress_raw(&with_bounds));
     CHECK(with_bounds.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
     // Re-create the bounded capability and assert that the current pesbt values matches that one.
-    auto new_cap = Handler::make_max_perms_cap(with_bounds.base(), with_bounds.address(), with_bounds.top());
-    CHECK(new_cap == with_bounds);
-    CHECK(new_cap.cr_pesbt == with_bounds.cr_pesbt);
-    CHECK(new_cap.cr_pesbt == Handler::compress_raw(&with_bounds));
-    CHECK(new_cap.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
+    if (with_bounds.cr_tag) {
+        auto new_cap = Handler::make_max_perms_cap(with_bounds.base(), with_bounds.address(), with_bounds.top());
+        CHECK(new_cap == with_bounds);
+        CHECK(new_cap.cr_pesbt == with_bounds.cr_pesbt);
+        CHECK(new_cap.cr_pesbt == Handler::compress_raw(&with_bounds));
+        CHECK(new_cap.cr_pesbt == Handler::sail_compress_raw(&with_bounds));
+    }
 
     if (was_exact)
         *was_exact = exact;

commit 38e5ba34cd1815fcacbd1dd1a4370caa3fcd1df6

Change the setbounds API to match the instructions
Being able to pass requested top and base is somewhat error-prone as
for Morello the masking of high bits needs to be taken into account.
Hide this implementation detail by passing a length instead.
This is the first step towards avoiding problems like the QEMU regression
introduced in commit 1a3898fa066e9f7e0560ccc95731d5b7f3a7c882 which caused
the Morello testsuite to fail.

cheri_compressed_cap_common.h
@@ -827,8 +827,10 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
 }
 
 /* @return whether the operation was able to set precise bounds precise or not */
-static inline bool _cc_N(setbounds)(_cc_cap_t* cap, _cc_addr_t req_base, _cc_length_t req_top) {
-    return _cc_N(setbounds_impl)(cap, req_base, req_top, NULL);
+static inline bool _cc_N(setbounds)(_cc_cap_t* cap, _cc_length_t req_len) {
+    // TODO: this is not quite right for Morello
+    _cc_length_t req_top = (_cc_length_t)cap->_cr_cursor + req_len;
+    return _cc_N(setbounds_impl)(cap, cap->_cr_cursor, req_top, NULL);
 }
 
 static inline _cc_cap_t _cc_N(make_max_perms_cap)(_cc_addr_t base, _cc_addr_t cursor, _cc_length_t top) {
@@ -911,9 +913,7 @@ public:
         _cc_N(decompress_mem)(pesbt, cursor, tag, cdp);
     }
     static inline bounds_bits extract_bounds_bits(addr_t pesbt) { return _cc_N(extract_bounds_bits)(pesbt); }
-    static inline bool setbounds(cap_t* cap, addr_t req_base, length_t req_top) {
-        return _cc_N(setbounds)(cap, req_base, req_top);
-    }
+    static inline bool setbounds(cap_t* cap, length_t req_len) { return _cc_N(setbounds)(cap, req_len); }
     static inline bool is_representable_cap_exact(const cap_t* cap) { return _cc_N(is_representable_cap_exact)(cap); }
     static inline cap_t make_max_perms_cap(addr_t base, addr_t cursor, length_t top) {
         return _cc_N(make_max_perms_cap)(base, cursor, top);

test/fuzz_decompress.cpp
@@ -102,14 +102,15 @@ static inline void check_crrl_and_cram(_cc_addr_t value) {
     }
 }
 
-void fuzz_setbounds(const _cc_cap_t& input_cap, _cc_addr_t req_base, _cc_addr_t req_len) {
+void fuzz_setbounds(const _cc_cap_t& input_cap, _cc_addr_t req_len) {
     _cc_cap_t new_result = input_cap;
     new_result.cr_tag = false;
     _cc_cap_t new_sail_result = new_result;
-    _cc_N(setbounds)(&new_result, req_base, (_cc_length_t)req_base + req_len);
-    _cc_sail(setbounds)(&new_sail_result, req_base, (_cc_length_t)req_base + req_len);
+    _cc_N(setbounds)(&new_result, req_len);
+    _cc_sail(setbounds)(&new_sail_result, req_len);
     if (!compare_caps("SETBOUNDS", new_result, new_sail_result)) {
-        fprintf(stderr, "with req_base=%" PRIx64 " and req_len=%" PRIx64 "\n", (uint64_t)req_base, (uint64_t)req_len);
+        fprintf(stderr, "with addr=%" PRIx64 " and req_len=%" PRIx64 "\n", (uint64_t)input_cap.address(),
+                (uint64_t)req_len);
         dump_cap_fields(input_cap);
         abort();
     }
@@ -185,9 +186,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 // TODO: Implement for Morello
 #ifndef TEST_CC_IS_MORELLO
     // Try running setbounds (on an untagged capability) and compare to sail.
-    fuzz_setbounds(result, /*req_base=*/result.base(), /*req_len=*/new_len);
-    fuzz_setbounds(result, /*req_base=*/result.address(), /*req_len=*/new_len);
-    fuzz_setbounds(result, /*req_base=*/random_base, /*req_len=*/new_len);
+    fuzz_setbounds(result, new_len);
+    _cc_N(set_addr)(&result, result.base());
+    fuzz_setbounds(result, new_len);
+    _cc_N(set_addr)(&result, random_base);
+    fuzz_setbounds(result, new_len);
 #endif
     return 0; // Non-zero return values are reserved for future use.
 }

test/sail_wrapper.h
@@ -46,7 +46,7 @@ bool sail_precise_is_representable_64(const cc64_cap_t* cap, cc64_addr_t new_add
 cc64_addr_t sail_null_pesbt_64(void);
 cc64_addr_t sail_representable_mask_64(cc64_addr_t len);
 cc64_addr_t sail_representable_length_64(cc64_addr_t len);
-bool sail_setbounds_64(cc64_cap_t* cap, cc64_addr_t req_base, cc64_length_t req_top);
+bool sail_setbounds_64(cc64_cap_t* cap, cc64_length_t req_len);
 cc64_cap_t sail_reset_capability_64(void);
 
 void sail_decode_128_mem(cc128_addr_t pesbt, cc128_addr_t cursor, bool tag, cc128_cap_t* cdp);
@@ -59,7 +59,7 @@ bool sail_precise_is_representable_128(const cc128_cap_t* cap, cc128_addr_t new_
 cc128_addr_t sail_null_pesbt_128(void);
 cc128_addr_t sail_representable_mask_128(cc128_addr_t len);
 cc128_addr_t sail_representable_length_128(cc128_addr_t len);
-bool sail_setbounds_128(cc128_cap_t* cap, cc128_addr_t req_base, cc128_length_t req_top);
+bool sail_setbounds_128(cc128_cap_t* cap, cc128_length_t req_len);
 cc128_cap_t sail_reset_capability_128(void);
 
 void sail_decode_128m_mem(cc128m_addr_t pesbt, cc128m_addr_t cursor, bool tag, cc128m_cap_t* cdp);
@@ -72,7 +72,7 @@ bool sail_precise_is_representable_128m(const cc128m_cap_t* cap, cc128m_addr_t n
 cc128m_addr_t sail_null_pesbt_128m(void);
 cc128m_addr_t sail_representable_mask_128m(cc128m_addr_t len);
 cc128m_addr_t sail_representable_length_128m(cc128m_addr_t len);
-bool sail_setbounds_128m(cc128m_cap_t* cap, cc128m_addr_t req_base, cc128m_length_t req_top);
+bool sail_setbounds_128m(cc128m_cap_t* cap, cc128m_length_t req_len);
 cc128m_cap_t sail_reset_capability_128m(void);
 
 #define _cc_sail_decode_mem _CC_CONCAT(_CC_CONCAT(sail_decode_, CC_FORMAT_LOWER), _mem)
@@ -104,9 +104,7 @@ public:
     static inline addr_t sail_null_pesbt() { return sail_null_pesbt_64(); }
     static inline addr_t sail_representable_mask(addr_t len) { return sail_representable_mask_64(len); }
     static inline addr_t sail_representable_length(addr_t len) { return sail_representable_length_64(len); }
-    static inline bool sail_setbounds(cap_t* cap, addr_t req_base, length_t req_top) {
-        return sail_setbounds_64(cap, req_base, req_top);
-    }
+    static inline bool sail_setbounds(cap_t* cap, length_t req_len) { return sail_setbounds_64(cap, req_len); }
     static inline cap_t sail_reset_capability() { return sail_reset_capability_64(); }
 };
 
@@ -130,9 +128,7 @@ public:
     static inline addr_t sail_null_pesbt() { return sail_null_pesbt_128(); }
     static inline addr_t sail_representable_mask(addr_t len) { return sail_representable_mask_128(len); }
     static inline addr_t sail_representable_length(addr_t len) { return sail_representable_length_128(len); }
-    static inline bool sail_setbounds(cap_t* cap, addr_t req_base, length_t req_top) {
-        return sail_setbounds_128(cap, req_base, req_top);
-    }
+    static inline bool sail_setbounds(cap_t* cap, length_t req_len) { return sail_setbounds_128(cap, req_len); }
     static inline cap_t sail_reset_capability() { return sail_reset_capability_128(); }
 };
 
@@ -156,9 +152,7 @@ public:
     static inline addr_t sail_null_pesbt() { return sail_null_pesbt_128m(); }
     static inline addr_t sail_representable_mask(addr_t len) { return sail_representable_mask_128m(len); }
     static inline addr_t sail_representable_length(addr_t len) { return sail_representable_length_128m(len); }
-    static inline bool sail_setbounds(cap_t* cap, addr_t req_base, length_t req_top) {
-        return sail_setbounds_128m(cap, req_base, req_top);
-    }
+    static inline bool sail_setbounds(cap_t* cap, addr_t req_len) { return sail_setbounds_128m(cap, req_len); }
     static inline cap_t sail_reset_capability() { return sail_reset_capability_128m(); }
 };
 

test/sail_wrapper_128m.c
@@ -96,7 +96,7 @@ struct cc128m_bounds_bits sail_extract_bounds_bits_128m(uint64_t pesbt) {
 uint64_t sail_compress_128m_raw(const cc128m_cap_t* csp) { return sail_compress_common_raw(csp); }
 uint64_t sail_compress_128m_mem(const cc128m_cap_t* csp) { return sail_compress_common_mem(csp); }
 
-bool sail_setbounds_128m(cc128m_cap_t* cap, cc128m_addr_t req_base, cc128m_length_t req_top) {
+bool sail_setbounds_128m(cc128m_cap_t* cap, cc128m_length_t req_len) {
     abort(); // TODO: call sailgen_CapSetBounds();
 }
 

test/sail_wrapper_common.c
@@ -310,7 +310,9 @@ _cc_addr_t _CC_CONCAT(sail_null_pesbt_, SAIL_WRAPPER_CC_FORMAT_LOWER)(void) {
     return null_pesbt;
 }
 
-bool _CC_CONCAT(sail_setbounds_, SAIL_WRAPPER_CC_FORMAT_LOWER)(_cc_cap_t* cap, _cc_addr_t req_base, _cc_length_t req_top) {
+bool _CC_CONCAT(sail_setbounds_, SAIL_WRAPPER_CC_FORMAT_LOWER)(_cc_cap_t* cap, _cc_length_t req_len) {
+    _cc_addr_t req_base = cap->_cr_cursor;
+    _cc_length_t req_top = (_cc_length_t)cap->_cr_cursor + req_len;
     struct zCapability sailcap = cap_t_to_sail_cap(cap);
     sail_cap_bits sailtop;
     CREATE(sail_cap_bits)(&sailtop);

test/setbounds_inputs_128.cpp
@@ -3,8 +3,10 @@
 struct setbounds_input {
     uint64_t base1;
     uint64_t top1;
+    uint64_t len1() const { return top1 - base1; }
     uint64_t base2;
     uint64_t top2;
+    uint64_t len2() const { return top2 - base2; }
     uint64_t sail_cc_base1;
     uint64_t sail_cc_top1;
     uint64_t sail_cc_base2;

test/setbounds_test_128.cpp
@@ -74,23 +74,25 @@ TEST_CASE("Fuzzer assertion exact untagged", "[fuzz]") {
     REQUIRE(decoded.top() == top);
     REQUIRE(decoded.address() == addr);
     {
-        auto sail_setbounds_base_top = decoded;
-        auto cc_setbounds_base_top = decoded;
-        TestAPICC::sail_setbounds(&sail_setbounds_base_top, base, top);
-        TestAPICC::setbounds(&cc_setbounds_base_top, base, top);
+        TestAPICC::cap_t tmp = decoded;
+        _cc_N(set_addr)(&tmp, base);
+        TestAPICC::cap_t sail_setbounds_base_top = tmp;
+        TestAPICC::cap_t cc_setbounds_base_top = tmp;
+        TestAPICC::sail_setbounds(&sail_setbounds_base_top, decoded.length());
+        TestAPICC::sail_setbounds(&cc_setbounds_base_top, decoded.length());
         CHECK(sail_setbounds_base_top == cc_setbounds_base_top);
     }
     {
         auto sail_setbounds_addr_top = decoded;
         auto cc_setbounds_addr_top = decoded;
-        TestAPICC::sail_setbounds(&sail_setbounds_addr_top, addr, top);
-        TestAPICC::setbounds(&cc_setbounds_addr_top, addr, top);
+        TestAPICC::sail_setbounds(&sail_setbounds_addr_top, decoded.top() - decoded.address());
+        TestAPICC::sail_setbounds(&cc_setbounds_addr_top, decoded.top() - decoded.address());
         CHECK(cc_setbounds_addr_top == cc_setbounds_addr_top);
     }
 
-    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+    do_csetbounds<TestAPICC>(decoded, nullptr, decoded.length());
     decoded._cr_cursor = base;
-    do_csetbounds<TestAPICC>(decoded, decoded.top(), nullptr);
+    do_csetbounds<TestAPICC>(decoded, nullptr, decoded.length());
 }
 
 TEST_CASE("TestRIG assertion untagged max cap", "[fuzz]") {
@@ -102,15 +104,16 @@ TEST_CASE("TestRIG assertion untagged max cap", "[fuzz]") {
 
     auto sail_setbounds_result = cap;
     auto cc_setbounds_result = cap;
-    const TestAPICC::length_t req_top = (TestAPICC::length_t)cap.address() + req_len;
-    TestAPICC::sail_setbounds(&sail_setbounds_result, cap.address(), req_top);
-    TestAPICC::setbounds(&cc_setbounds_result, cap.address(), req_top);
+    TestAPICC::sail_setbounds(&sail_setbounds_result, req_len);
+    TestAPICC::setbounds(&cc_setbounds_result, req_len);
     CHECK(sail_setbounds_result == cc_setbounds_result);
-
-    do_csetbounds<TestAPICC>(cap, req_len, nullptr);
+    // The requested top of 0x1fffffffff4232000 will be rounded to 0x20000000000000000 which is not
+    // representable with the 65-bit top and is therefore truncated to zero.
+    CHECK(sail_setbounds_result.top() == 0);
+    do_csetbounds<TestAPICC>(cap, nullptr, req_len);
     auto cap2 = cap;
     cap2._cr_cursor = cap.base();
-    do_csetbounds<TestAPICC>(cap2, req_len, nullptr);
+    do_csetbounds<TestAPICC>(cap2, nullptr, req_len);
 }
 
 TEST_CASE("TestRIG setbounds result mismatch", "[fuzz]") {
@@ -126,14 +129,13 @@ TEST_CASE("TestRIG setbounds result mismatch", "[fuzz]") {
 
     auto sail_setbounds_result = cap;
     auto cc_setbounds_result = cap;
-    const TestAPICC::length_t req_top = (TestAPICC::length_t)cap.address() + req_len;
-    TestAPICC::sail_setbounds(&sail_setbounds_result, cap.address(), req_top);
-    TestAPICC::setbounds(&cc_setbounds_result, cap.address(), req_top);
+    TestAPICC::sail_setbounds(&sail_setbounds_result, req_len);
+    TestAPICC::setbounds(&cc_setbounds_result, req_len);
     CHECK(sail_setbounds_result == cc_setbounds_result);
     REQUIRE(cc_setbounds_result.base() == UINT64_C(0xFFFFFFFFFFFFF934));
     REQUIRE(cc_setbounds_result.address() == UINT64_C(0xFFFFFFFFFFFFF934));
     REQUIRE(cc_setbounds_result.top() == CC128_MAX_TOP + 0b10111100101);
     REQUIRE(cc_setbounds_result.length() == req_len);
 
-    check_csetbounds_invariants<TestAPICC>(cap, cc_setbounds_result, true, cap.address(), req_top);
+    check_csetbounds_invariants<TestAPICC>(cap, cc_setbounds_result, true, req_len);
 }

test/setbounds_test_common.cpp
@@ -25,8 +25,10 @@ struct setbounds_regressions {
 template <typename Handler>
 static inline void check_csetbounds_invariants(const typename Handler::cap_t& initial_cap,
                                                const typename Handler::cap_t& with_bounds, bool was_exact,
-                                               _cc_addr_t requested_base, typename Handler::length_t requested_top) {
+                                               typename Handler::length_t requested_len) {
     CAPTURE(initial_cap, with_bounds, was_exact);
+    typename Handler::addr_t requested_base = initial_cap.address();
+    typename Handler::length_t requested_top = initial_cap.address() + requested_len;
     // Address should be the same!
     REQUIRE(with_bounds.address() == initial_cap.address());
     if (was_exact) {
@@ -34,7 +36,11 @@ static inline void check_csetbounds_invariants(const typename Handler::cap_t& in
         REQUIRE(with_bounds.top() == requested_top);
     } else {
         REQUIRE(with_bounds.base() <= requested_base); // base must not be greater than what we asked for
-        REQUIRE(with_bounds.top() >= requested_top);   // top must not be less than what we asked for
+        if (requested_top > _CC_N(MAX_LENGTH)) {
+            REQUIRE(!with_bounds.cr_tag);
+        } else {
+            REQUIRE(with_bounds.top() >= requested_top); // top must not be less than what we asked for
+        }
         // At least one must be different otherwise was_exact is not correct
         REQUIRE((with_bounds.top() != requested_top || with_bounds.base() != requested_base));
     }
@@ -46,21 +52,21 @@ static inline void check_csetbounds_invariants(const typename Handler::cap_t& in
 }
 
 template <typename Handler>
-static typename Handler::cap_t do_csetbounds(const typename Handler::cap_t& initial_cap,
-                                             typename Handler::length_t requested_top, bool* was_exact) {
+static typename Handler::cap_t do_csetbounds(const typename Handler::cap_t& initial_cap, bool* was_exact,
+                                             typename Handler::length_t requested_len) {
     typename Handler::cap_t with_bounds = initial_cap;
     typename Handler::addr_t requested_base = initial_cap.address();
-    CAPTURE(initial_cap, requested_top, requested_base);
-    bool exact = Handler::setbounds(&with_bounds, requested_base, requested_top);
+    CAPTURE(initial_cap, requested_len, requested_base);
+    bool exact = Handler::setbounds(&with_bounds, requested_len);
     CAPTURE(with_bounds, exact);
 
     typename Handler::cap_t sail_with_bounds = initial_cap;
-    bool sail_exact = Handler::sail_setbounds(&sail_with_bounds, requested_base, requested_top);
+    bool sail_exact = Handler::sail_setbounds(&sail_with_bounds, requested_len);
     CAPTURE(sail_with_bounds, sail_exact);
 
     CHECK(sail_with_bounds == with_bounds);
     CHECK(sail_exact == exact);
-    check_csetbounds_invariants<Handler>(initial_cap, with_bounds, exact, requested_base, requested_top);
+    check_csetbounds_invariants<Handler>(initial_cap, with_bounds, exact, requested_len);
 
     // Check that the cr_pesbt is updated correctly and matches sail
     CHECK(with_bounds.cr_pesbt == Handler::compress_raw(&with_bounds));
@@ -82,9 +88,8 @@ static typename Handler::cap_t do_csetbounds(const typename Handler::cap_t& init
 template <typename Handler>
 static _cc_cap_t check_bounds_exact(const typename Handler::cap_t& initial_cap,
                                     typename Handler::length_t requested_length, bool should_be_exact) {
-    typename Handler::length_t req_top = initial_cap.address() + (typename Handler::length_t)requested_length;
     bool exact = false;
-    _cc_cap_t with_bounds = do_csetbounds<Handler>(initial_cap, req_top, &exact);
+    _cc_cap_t with_bounds = do_csetbounds<Handler>(initial_cap, &exact, requested_length);
     CHECK(exact == should_be_exact);
     return with_bounds;
 }
@@ -110,12 +115,12 @@ static inline void check_cram_matches_setbounds(_cc_length_t req_top, const _cc_
 }
 
 template <typename Handler>
-void test_zero_length_one_past_end() { // Create an out-of-bounds capability and set length to one
+void test_zero_length_one_past_end() { // Create an out-of-bounds capability and set length to zero
     constexpr unsigned base = 12345;
     constexpr unsigned base_plus_one = base + 1;
     auto one_past_end = Handler::make_max_perms_cap(base, base_plus_one, base_plus_one);
     bool exact;
-    auto result = do_csetbounds<Handler>(one_past_end, base_plus_one, &exact);
+    auto result = do_csetbounds<Handler>(one_past_end, &exact, 0);
     CHECK(result.base() == base_plus_one);
     CHECK(result.top() == base_plus_one);
     CHECK(result.address() == base_plus_one);
@@ -123,7 +128,7 @@ void test_zero_length_one_past_end() { // Create an out-of-bounds capability and
     CHECK(exact);
     // Check that calling setbounds with the same target top yields the same result
     bool same_again_exact = false;
-    auto same_again = do_csetbounds<Handler>(result, base_plus_one, &same_again_exact);
+    auto same_again = do_csetbounds<Handler>(result, &same_again_exact, 0);
     CHECK(result == same_again);
 }
 
@@ -140,7 +145,7 @@ TEST_CASE("setbounds test cases from sail", "[bounds]") {
         REQUIRE(first_input.base() == 0);
         REQUIRE(first_input.top() == _CC_N(MAX_LENGTH));
         bool first_exact = false;
-        const _cc_cap_t first_bounds = do_csetbounds<Handler>(first_input, input.top1, &first_exact);
+        const _cc_cap_t first_bounds = do_csetbounds<Handler>(first_input, &first_exact, input.len1());
         CHECK(first_bounds.base() == input.sail_cc_base1);
         CHECK(first_bounds.top() == input.sail_cc_top1);
         // Check CRAP/CRAM:
@@ -148,7 +153,7 @@ TEST_CASE("setbounds test cases from sail", "[bounds]") {
 
         // Check that calling setbounds with the same target top yields the same result
         bool first_again_exact = false;
-        const _cc_cap_t first_bounds_again = do_csetbounds<Handler>(first_bounds, input.top1, &first_again_exact);
+        const _cc_cap_t first_bounds_again = do_csetbounds<Handler>(first_bounds, &first_again_exact, input.len1());
         CHECK(first_again_exact == first_exact);
         CHECK(first_bounds.base() == first_bounds_again.base());
         CHECK(first_bounds.top() == first_bounds_again.top());
@@ -161,7 +166,7 @@ TEST_CASE("setbounds test cases from sail", "[bounds]") {
         second_input._cr_cursor += input.base2 - second_input.address();
         REQUIRE(second_input.address() == input.base2);
         bool second_exact = false;
-        const _cc_cap_t second_bounds = do_csetbounds<Handler>(second_input, input.top2, &second_exact);
+        const _cc_cap_t second_bounds = do_csetbounds<Handler>(second_input, &second_exact, input.len2());
         CHECK(second_bounds.base() == input.sail_cc_base2);
         CHECK(second_bounds.top() == input.sail_cc_top2);
         // Check CRAP/CRAM:
@@ -173,8 +178,7 @@ TEST_CASE("setbounds test cases from sail", "[bounds]") {
         }
         // Check that calling setbounds with the same target top yields the same result
         bool second_again_exact = false;
-        const _cc_cap_t second_bounds_again =
-            do_csetbounds<Handler>(second_bounds, input.top2, &second_again_exact);
+        const _cc_cap_t second_bounds_again = do_csetbounds<Handler>(second_bounds, &second_again_exact, input.len2());
         CHECK(second_again_exact == second_exact);
         CHECK(second_bounds.base() == second_bounds_again.base());
         CHECK(second_bounds.top() == second_bounds_again.top());