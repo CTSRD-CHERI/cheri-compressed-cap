11c2af5d199296f74e76bdcb1e552128a4384a3a
Martin Kaiser <martin.kaiser@codasip.com>
2024-04-30 14:21:42 +0200

    Initial version of new 64r format
    This is an initial draft of the 64r capability format for 64-bit
    capabilities as defined in the RISC-V CHERI specification.
    
    Note that this does not handle the decoding of raw permissions to the
    architectural ones.
    Future commits will add a new encode and decode function, but for now
    just set CC64R_PERMS_ALL to the infinite cap value.
    
    Co-authored-by: Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>

--- cheri_compressed_cap_64r.h
+++ cheri_compressed_cap_64r.h
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2018-2025 Alex Richardson
+ * Copyright (c) 2024 Codasip
  *
  * This software was developed by SRI International and the University of
  * Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
@@ -37,127 +38,120 @@
 // CHERI compressed capability format for the RISC-V standard
 
 // The following macros are expected to be defined
-#define CC_FORMAT_LOWER 128r
-#define CC_FORMAT_UPPER 128R
-#define CC128R_CAP_SIZE 16
-#define CC128R_CAP_BITS 128
-#define CC128R_ADDR_WIDTH 64
-#define CC128R_LEN_WIDTH 65
+#define CC_FORMAT_LOWER 64r
+#define CC_FORMAT_UPPER 64R
+#define CC64R_CAP_SIZE 8
+#define CC64R_CAP_BITS 64
+#define CC64R_ADDR_WIDTH 32
+#define CC64R_LEN_WIDTH 33
 
-#define CC128R_MANTISSA_WIDTH 14
+#define CC64R_MANTISSA_WIDTH 10
 // Max exponent is the largest exponent _required_, not that can be encoded.
-#define CC128R_MAX_EXPONENT 52
-#define CC128R_CURSOR_MASK 0xFFFFFFFFFFFFFFFF
-#define CC128R_MAX_ADDRESS_PLUS_ONE ((cc128r_length_t)1u << CC128R_ADDR_WIDTH)
-#define CC128R_NULL_TOP CC128R_MAX_ADDRESS_PLUS_ONE
-#define CC128R_NULL_LENGTH CC128R_MAX_ADDRESS_PLUS_ONE
-#define CC128R_MAX_LENGTH CC128R_MAX_ADDRESS_PLUS_ONE
-#define CC128R_MAX_TOP CC128R_MAX_ADDRESS_PLUS_ONE
-#define CC128R_MAX_ADDR UINT64_MAX
-/* Special otypes are allocated upwards from 0 */
-#define CC128R_SPECIAL_OTYPE_VAL(val) (val##u)
-#define CC128R_SPECIAL_OTYPE_VAL_SIGNED(val) (val##u)
+#define CC64R_MAX_EXPONENT 24
+#define CC64R_CURSOR_MASK 0xFFFFFFFF
+#define CC64R_MAX_ADDRESS_PLUS_ONE ((cc64r_length_t)1u << CC64R_ADDR_WIDTH)
+#define CC64R_NULL_TOP CC64R_MAX_ADDRESS_PLUS_ONE
+#define CC64R_NULL_LENGTH CC64R_MAX_ADDRESS_PLUS_ONE
+#define CC64R_MAX_LENGTH CC64R_MAX_ADDRESS_PLUS_ONE
+#define CC64R_MAX_TOP CC64R_MAX_ADDRESS_PLUS_ONE
+#define CC64R_MAX_ADDR UINT32_MAX
 
-/* Use __uint128 to represent 65 bit length */
-__extension__ typedef unsigned __int128 cc128r_length_t;
-__extension__ typedef signed __int128 cc128r_offset_t;
-typedef uint64_t cc128r_addr_t;
-typedef int64_t cc128r_saddr_t;
+/* Special otypes are allocated upwards from 0 */
+#define CC64R_SPECIAL_OTYPE_VAL(val) (val##u)
+#define CC64R_SPECIAL_OTYPE_VAL_SIGNED(val) (val##u)
+
+/* Use uint64_t to represent 33 bit length */
+typedef uint64_t cc64r_length_t;
+typedef int64_t cc64r_offset_t;
+typedef uint32_t cc64r_addr_t;
+typedef int32_t cc64r_saddr_t;
 #include "cheri_compressed_cap_macros.h"
 
 /* ignore ISO C restricts enumerator values to range of 'int' */
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
 enum {
-    _CC_FIELD(RESERVED1, 127, 121),
-    _CC_FIELD(UPERMS, 120, 117),
-    _CC_FIELD(FLAGS, 115, 116),
-    _CC_FIELD(HWPERMS, 116, 108),
-    // TODO: Level should be separate: _CC_FIELD(LEVEL, 107, 107)
-    _CC_FIELD(RESERVED0, 107, 92),
-    _CC_FIELD(OTYPE, 91, 91),
-    _CC_FIELD(EBT, 90, 64),
+    _CC_FIELD(UPERMS, 63, 62),
+    _CC_FIELD(HWPERMS, 61, 57),
+    _CC_FIELD(FLAGS, 57, 57), // For now pretend that the M bit is always present, not just for X caps
+    // TODO: Level should be separate: _CC_FIELD(LEVEL, 56, 56)
+    _CC_FIELD(RESERVED, 55, 53),
+    _CC_FIELD(OTYPE, 52, 52),
+    _CC_FIELD(EBT, 51, 32),
 
-    _CC_FIELD(EXPONENT_FORMAT, 90, 90),
+    _CC_FIELD(EXPONENT_FORMAT, 51, 51),
+    _CC_FIELD(LEN_MSB, 50, 50),
     // The FIELD_INTERNAL_EXPONENT_SIZE name is currently required by various static assertions.
     _CC_N(FIELD_INTERNAL_EXPONENT_SIZE) = _CC_N(FIELD_EXPONENT_FORMAT_SIZE),
-    _CC_FIELD(TOP_ENCODED, 89, 78),
-    _CC_FIELD(BOTTOM_ENCODED, 77, 64),
+    _CC_FIELD(TOP_ENCODED, 49, 42),
+    _CC_FIELD(BOTTOM_ENCODED, 41, 32),
 
-    // Top/bottom offsets depending in INTERNAL_EXPONENT flag:
+    // Top/bottom offsets depending on INTERNAL_EXPONENT flag:
     // Without internal exponent:
-    _CC_FIELD(EXP_ZERO_TOP, 89, 78),
-    _CC_FIELD(EXP_ZERO_BOTTOM, 77, 64),
+    _CC_FIELD(EXP_ZERO_TOP, 49, 42),
+    _CC_FIELD(EXP_ZERO_BOTTOM, 41, 32),
     // With internal exponent:
-    _CC_FIELD(EXP_NONZERO_TOP, 89, 81),
-    _CC_FIELD(EXPONENT_HIGH_PART, 80, 78),
-    _CC_FIELD(EXP_NONZERO_BOTTOM, 77, 67),
-    _CC_FIELD(EXPONENT_LOW_PART, 66, 64),
+    _CC_FIELD(EXP_NONZERO_TOP, 49, 44),
+    _CC_FIELD(EXPONENT_HIGH_PART, 43, 42),
+    _CC_FIELD(EXP_NONZERO_BOTTOM, 41, 34),
+    _CC_FIELD(EXPONENT_LOW_PART, 33, 32),
 };
 #pragma GCC diagnostic pop
 
-#define CC128R_OTYPE_BITS CC128R_FIELD_OTYPE_SIZE
-#define CC128R_BOT_WIDTH CC128R_FIELD_EXP_ZERO_BOTTOM_SIZE
-#define CC128R_BOT_INTERNAL_EXP_WIDTH CC128R_FIELD_EXP_NONZERO_BOTTOM_SIZE
-#define CC128R_EXP_LOW_WIDTH CC128R_FIELD_EXPONENT_LOW_PART_SIZE
+#define CC64R_OTYPE_BITS CC64R_FIELD_OTYPE_SIZE
+#define CC64R_BOT_WIDTH CC64R_FIELD_EXP_ZERO_BOTTOM_SIZE
+#define CC64R_BOT_INTERNAL_EXP_WIDTH CC64R_FIELD_EXP_NONZERO_BOTTOM_SIZE
+#define CC64R_EXP_LOW_WIDTH CC64R_FIELD_EXPONENT_LOW_PART_SIZE
 
-#define CC128R_PERM_CAPABILITY (1 << 0)
-#define CC128R_PERM_WRITE (1 << 1)
-#define CC128R_PERM_READ (1 << 2)
-#define CC128R_PERM_EXECUTE (1 << 3)
-#define CC128R_PERM_ACCESS_SYS_REGS (1 << 4)
-#define CC128R_PERM_LOAD_MUTABLE (1 << 5)
-#define CC128R_PERM_ELEVATE_LEVEL (1 << 6)
-#define CC128R_PERM_STORE_LEVEL (1 << 7)
+#define CC64R_PERM_CAPABILITY (1 << 0)
+#define CC64R_PERM_WRITE (1 << 1)
+#define CC64R_PERM_READ (1 << 2)
+#define CC64R_PERM_EXECUTE (1 << 3)
+#define CC64R_PERM_ACCESS_SYS_REGS (1 << 4)
+#define CC64R_PERM_LOAD_MUTABLE (1 << 5)
+#define CC64R_PERM_ELEVATE_LEVEL (1 << 6)
+#define CC64R_PERM_STORE_LEVEL (1 << 7)
 
 // Note: shift by one 1 due to level currently being part of perms
-#define CC128R_HIGHEST_PERM (CC128R_PERM_STORE_LEVEL << 1)
+#define CC64R_HIGHEST_PERM (CC64R_PERM_STORE_LEVEL << 1)
 
-_CC_STATIC_ASSERT(CC128R_HIGHEST_PERM < CC128R_FIELD_HWPERMS_MAX_VALUE, "permissions not representable?");
-_CC_STATIC_ASSERT((CC128R_HIGHEST_PERM << 1) > CC128R_FIELD_HWPERMS_MAX_VALUE, "all permission bits should be used");
-
-#define CC128R_PERMS_ALL (0x13f) /* 0b100111111 since SL and EL are not supported in sail yet. */
-#define CC128R_UPERMS_ALL (0xf)  /* 4 bits */
-_CC_STATIC_ASSERT_SAME(CC128R_PERMS_ALL,
-                       CC128R_FIELD_HWPERMS_MAX_VALUE & ~(CC128R_PERM_ELEVATE_LEVEL | CC128R_PERM_STORE_LEVEL));
-_CC_STATIC_ASSERT_SAME(CC128R_UPERMS_ALL, CC128R_FIELD_UPERMS_MAX_VALUE);
-#define CC128R_UPERMS_SHFT (6)
-#define CC128R_UPERMS_MEM_SHFT (0)
-#define CC128R_MAX_UPERM (3)
+#define CC64R_PERMS_ALL (0x9)  // See Infinite Capability in the spec: 0x8 or 0x9 depending on mode.
+#define CC64R_UPERMS_ALL (0x3) // 2 bits
+_CC_STATIC_ASSERT_SAME(CC64R_UPERMS_ALL, CC64R_FIELD_UPERMS_MAX_VALUE);
+#define CC64R_UPERMS_SHFT (6)
+#define CC64R_UPERMS_MEM_SHFT (0)
+#define CC64R_MAX_UPERM (1)
 
 // Currently, only one type (sentry) is defined.
 // However, other extensions (e.g. CHERIoT) define additional otypes beyond this.
 enum _CC_N(OTypes) {
-    CC128R_MAX_REPRESENTABLE_OTYPE = ((1u << CC128R_OTYPE_BITS) - 1u),
+    CC64R_MAX_REPRESENTABLE_OTYPE = ((1u << CC64R_OTYPE_BITS) - 1u),
     _CC_SPECIAL_OTYPE(OTYPE_UNSEALED, 0),
     _CC_SPECIAL_OTYPE(OTYPE_SENTRY, 1),
     _CC_N(MIN_RESERVED_OTYPE) = _CC_N(OTYPE_UNSEALED),
     _CC_N(MAX_RESERVED_OTYPE) = _CC_N(OTYPE_SENTRY),
 };
 
-#define CC128R_LS_SPECIAL_OTYPES(ITEM, ...)                                                                            \
+#define CC64R_LS_SPECIAL_OTYPES(ITEM, ...)                                                                             \
     ITEM(OTYPE_UNSEALED, __VA_ARGS__)                                                                                  \
     ITEM(OTYPE_SENTRY, __VA_ARGS__)
 
-_CC_STATIC_ASSERT_SAME(CC128R_MANTISSA_WIDTH, CC128R_FIELD_EXP_ZERO_BOTTOM_SIZE);
+_CC_STATIC_ASSERT_SAME(CC64R_MANTISSA_WIDTH, CC64R_FIELD_EXP_ZERO_BOTTOM_SIZE);
 
 // The RISC-V extension uses an "exponent zero" flag.
-#define CC128R_ENCODE_IE(IE) _CC_ENCODE_FIELD(!(IE), EXPONENT_FORMAT)
-#define CC128R_EXTRACT_IE(value) (!_CC_EXTRACT_FIELD(value, EXPONENT_FORMAT))
+#define CC64R_ENCODE_IE(IE) _CC_ENCODE_FIELD(!(IE), EXPONENT_FORMAT)
+#define CC64R_EXTRACT_IE(value) (!_CC_EXTRACT_FIELD(value, EXPONENT_FORMAT))
 // The exponent bits in memory are subtracted from the max exponent when decoding in the IE case.
-#define CC128R_ENCODE_EXPONENT(E) _CC_ENCODE_SPLIT_EXPONENT(CC128R_MAX_EXPONENT - (E))
-#define CC128R_EXTRACT_EXPONENT(pesbt) (CC128R_MAX_EXPONENT - _CC_EXTRACT_SPLIT_EXPONENT(pesbt))
-#define CC128R_RESERVED_FIELDS 2
-#define CC128R_RESERVED_BITS (CC128R_FIELD_RESERVED0_SIZE + CC128R_FIELD_RESERVED1_SIZE)
-#define CC128R_HAS_BASE_TOP_SPECIAL_CASES 1
-#define CC128R_USES_V9_CORRECTION_FACTORS 0
-#define CC128R_USES_LEN_MSB 0
+// FIXME: this is currently not correct, we are missing the L8 bit
+#define CC64R_ENCODE_EXPONENT(E) _CC_ENCODE_SPLIT_EXPONENT(CC64R_MAX_EXPONENT - (E))
+#define CC64R_EXTRACT_EXPONENT(pesbt) (CC64R_MAX_EXPONENT - _CC_EXTRACT_SPLIT_EXPONENT(pesbt))
+#define CC64R_RESERVED_FIELDS 1
+#define CC64R_RESERVED_BITS CC64R_FIELD_RESERVED_SIZE
+#define CC64R_HAS_BASE_TOP_SPECIAL_CASES 1
+#define CC64R_USES_V9_CORRECTION_FACTORS 0
+#define CC64R_USES_LEN_MSB 1
 
 #include "cheri_compressed_cap_common.h"
-
-static inline uint8_t cc128r_get_reserved(const cc128r_cap_t* cap) {
-    return _CC_EXTRACT_SPLIT_FIELD(cap->cr_pesbt, RESERVED1, RESERVED0);
-}
 
 static inline bool _cc_N(bounds_malformed)(_cc_bounds_bits bounds) {
     // The spec defines this check as checking for E < 0, but since we store it as an unsigned number, we compare it to
@@ -180,7 +174,7 @@ static inline bool _cc_N(compute_base_top_special_cases)(_cc_bounds_bits bounds,
 }
 
 // Check that there is no XOR mask for this encoding format (i.e. NULL encodes to all-zeroes in memory).
-_CC_STATIC_ASSERT_SAME(CC128R_MEM_XOR_MASK, UINT64_C(0));
+_CC_STATIC_ASSERT_SAME(CC64R_MEM_XOR_MASK, UINT64_C(0));
 
 #undef CC_FORMAT_LOWER
 #undef CC_FORMAT_UPPER
