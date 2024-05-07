ee462262cc69b83679092a3042188d9792a7423a
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-07 15:49:02 +0200

    Macros to generate AP test cases
    Define macros to generate test cases for risc-v cheri's AP field.
    
    Support tests where AP is set, cr_arch_perm is calculated and compared
    with an expected result. Add a second macro for the opposite direction
    where cr_arch_perm is set, AP is calculated and checked.
    
    Co-authored-by: Alexander Richardson <alexrichardson@google.com>

---
+++ test/cap_ap.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define CAP_AP_C _CC_N(PERM_CAPABILITY)
+#define CAP_AP_W _CC_N(PERM_WRITE)
+#define CAP_AP_R _CC_N(PERM_READ)
+#define CAP_AP_X _CC_N(PERM_EXECUTE)
+#define CAP_AP_ASR _CC_N(PERM_ACCESS_SYS_REGS)
+#define CAP_AP_LM _CC_N(PERM_LOAD_MUTABLE)
+#define CAP_AP_EL _CC_N(PERM_ELEVATE_LEVEL)
+#define CAP_AP_SL _CC_N(PERM_STORE_LEVEL)
+
+#define TEST_CASE_AP_COMP(_cr_arch_perm, _ap)                                                                          \
+    TEST_CASE(#_cr_arch_perm " compress", "[compress]") {                                                              \
+        _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);                                                           \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, HWPERMS) == 0);                                                          \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, UPERMS) == 0);                                                           \
+        CHECK(_cc_N(set_permissions)(&cap, (_cr_arch_perm)));                                                          \
+        CHECK((cap.all_permissions() & (_cr_arch_perm)) == (_cr_arch_perm));                                           \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, HWPERMS) == (_ap));                                                      \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, UPERMS) == 0);                                                           \
+    }
+
+#define TEST_CASE_AP_DECOMP(_ap, _cr_arch_perm)                                                                        \
+    TEST_CASE(#_ap " decompress", "[decompress]") {                                                                    \
+        _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);                                                           \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, HWPERMS) == 0);                                                          \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, UPERMS) == 0);                                                           \
+        cap.cr_pesbt = _CC_DEPOSIT_FIELD(cap.cr_pesbt, (_ap), HWPERMS);                                                \
+        CHECK((cap.all_permissions() & (_cr_arch_perm)) == (_cr_arch_perm));                                           \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, HWPERMS) == (_ap));                                                      \
+        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, UPERMS) == 0);                                                           \
+    }
