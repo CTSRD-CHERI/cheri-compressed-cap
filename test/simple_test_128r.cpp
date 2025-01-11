9e30061bac0c63b502368c14df2541a958b8686c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-01-10 17:16:37 -0800

    Fix correction factor R for RISC-V format
    We have to truncate the subtraction result to mantissa width instead of
    using a full 32-bit integer. Using a 32-bit integer resulted in the
    comparison with the other factors to almost always return 1.

--- test/simple_test_128r.cpp
+++ test/simple_test_128r.cpp
@@ -29,6 +29,20 @@ TEST_CASE("New adjustment factors", "[bounds]") {
     CHECK((int64_t)cap.offset() == -0xc72);
 }
 
+TEST_CASE("New adjustment factors 2", "[bounds]") {
+    // Regression test: the new format was still using the old V9 correctionTop algorithm.
+    // cursor: 0x3e6420dc58fe385b
+    auto cap = TestAPICC::decompress_raw(0x1f1da60bcff2c08e, 0x3e6420dc58fe385b, false);
+    auto sail_cap = TestAPICC::sail_decode_raw(0x1f1da60bcff2c08e, 0x3e6420dc58fe385b, false);
+    CHECK(cap == sail_cap);
+    // This previously reported base=0x3e6420dc58fe008e (008e instead of 408e)
+    CHECK(cap.base() == 0x3e6420dc58fe408e);
+    // This previously reported top=0x3e6420dc58fe0fcb (0fcb instead of 4fcb)
+    CHECK(cap.top() == 0x3e6420dc58fe4fcb);
+    // This previously reported offset=37cd
+    CHECK((int64_t)cap.offset() == -0x833);
+}
+
 TEST_CASE("Malformed bounds return zero", "[bounds]") {
     // Regression test: this pesbt value did not report malformed bounds
     auto cap = TestAPICC::decompress_raw(0x5042f7ed6b1027a9, 0, false);
