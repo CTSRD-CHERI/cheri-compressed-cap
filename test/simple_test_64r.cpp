172b6744962103e6e39870503bdeaae74a2c224e
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-28 10:17:13 -0800

    Bounds encoding test for 64r, T8 = 1
    Test the encoding of a capability from base, address and top for
    MXLEN=32. This test case uses an internal exponent such that T8 = 1.
    
    fixup test

--- test/simple_test_64r.cpp
+++ test/simple_test_64r.cpp
@@ -1,1 +1,27 @@
 #include "simple_test_common.cpp"
+
+TEST_CASE("bounds encoding, internal exponent, T8 = 1", "[bounds]") {
+    /* params are base, cursor, top */
+    _cc_cap_t cap = CompressedCap64r::make_max_perms_cap(0x0, 0x1000, 0x8000);
+
+    /*
+     * 11 SDP
+     * 01001 AP quadrant 1, almighty, integer mode
+     * 0000 reserved
+     * 0 S
+     * 0 EF
+     * 1 T8
+     * 000000 T[7:2]
+     * 00 TE
+     * 00000000 B[9:2]
+     * 01 BE
+     *
+     * internal exponent, E = 24 - 0b10001 = 7
+     * LCout = 0, LMSB = 1
+     * c_t = 0, c_b = 0
+     */
+    CHECK(cap.cr_pesbt == 0xd2040001);
+    cc64r_update_perms(&cap, 0);
+    cc64r_update_uperms(&cap, 0);
+    CHECK(cap.cr_pesbt == 0x00040001);
+}
