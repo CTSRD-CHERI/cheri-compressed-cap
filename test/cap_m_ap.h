7a1f16ce2412cbf670925a5a6841a94fac2fd4ae
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-03-07 15:16:57 -0800

    Prepare the AP (de)compression tests for levels support
    While doing this also move most of the function body outside of the
    macro and into a new function. This should improve compile time and
    makes it easier to edit.

--- test/cap_m_ap.h
+++ test/cap_m_ap.h
@@ -12,28 +12,50 @@
 #define LVB_0 0
 #define LVB_1 1
 
+static inline _cc_addr_t compute_supported_permissions(unsigned level_bits) {
+    _cc_addr_t supported_permissions = CAP_AP_C | CAP_AP_W | CAP_AP_R | CAP_AP_X | CAP_AP_ASR | CAP_AP_LM;
+    if (level_bits > 0)
+        supported_permissions |= CAP_AP_EL | CAP_AP_SL;
+    return supported_permissions;
+}
+
+static void testCompression(unsigned level_bits, _cc_mode mode, _cc_addr_t permissions, _cc_addr_t expected_encoding) {
+    // ensure we didn't request unsupported perms:
+    const _cc_addr_t supported_permissions = compute_supported_permissions(level_bits);
+    REQUIRE((permissions & supported_permissions) == permissions);
+    _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == 0);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);
+    CHECK(_cc_N(set_permissions)(&cap, permissions));
+    const bool has_x = cap.has_permissions(_CC_N(PERM_EXECUTE));
+    CHECK(_cc_N(set_execution_mode)(&cap, mode) == has_x);
+    CHECK(cap.has_permissions(permissions));
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == expected_encoding);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);
+}
+
+static void testDecompression(unsigned level_bits, _cc_addr_t ap_m_encoding, _cc_mode expected_mode,
+                              _cc_addr_t expected_permissions) {
+    // ensure we didn't request unsupported perms:
+    const _cc_addr_t supported_permissions = compute_supported_permissions(level_bits);
+    REQUIRE((expected_permissions & supported_permissions) == expected_permissions);
+    _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == 0);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);
+    cap.cr_pesbt = _CC_DEPOSIT_FIELD(cap.cr_pesbt, ap_m_encoding, AP_M);
+    CHECK(cap.has_permissions(expected_permissions));
+    CHECK((cap.all_permissions() & supported_permissions) == expected_permissions); // ignore reserved-one bits
+    CHECK(_cc_N(get_execution_mode)(&cap) == expected_mode);
+    CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == ap_m_encoding);
+    CHECK(cap.software_permissions() == 0); // should not have been set
+}
+
 #define TEST_CASE_M_AP_COMP(_cr_lvbits, _cr_m, _cr_arch_perm, _cr_ap_m)                                                \
     TEST_CASE(#_cr_lvbits "-" #_cr_arch_perm "-" #_cr_m " compress", "[compress]") {                                   \
-        _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);                                                           \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == 0);                                                             \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);                                                              \
-        CHECK(_cc_N(set_permissions)(&cap, (_cr_arch_perm)));                                                          \
-        const bool has_x = cap.has_permissions(_CC_N(PERM_EXECUTE));                                                   \
-        CHECK(_cc_N(set_execution_mode)(&cap, (_cc_mode)(_cr_m)) == has_x);                                            \
-        CHECK((cap.all_permissions() & (_cr_arch_perm)) == (_cr_arch_perm));                                           \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == (_cr_ap_m));                                                    \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);                                                              \
+        testCompression(_cr_lvbits, (_cc_mode)_cr_m, _cr_arch_perm, _cr_ap_m);                                         \
     }
 
 #define TEST_CASE_M_AP_DECOMP(_cr_lvbits, _cr_ap_m, _cr_m, _cr_arch_perm)                                              \
     TEST_CASE(#_cr_lvbits "-" #_cr_ap_m " decompress", "[decompress]") {                                               \
-        _cc_cap_t cap = TestAPICC::make_null_derived_cap(0);                                                           \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == 0);                                                             \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);                                                              \
-        _cc_addr_t ap_m = (_cr_ap_m);                                                                                  \
-        cap.cr_pesbt = _CC_DEPOSIT_FIELD(cap.cr_pesbt, ap_m, AP_M);                                                    \
-        CHECK((cap.all_permissions() & (_cr_arch_perm)) == (_cr_arch_perm));                                           \
-        CHECK(_cc_N(get_execution_mode)(&cap) == (_cc_mode)(_cr_m));                                                   \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, AP_M) == ap_m);                                                          \
-        CHECK(_CC_EXTRACT_FIELD(cap.cr_pesbt, SDP) == 0);                                                              \
+        testDecompression(_cr_lvbits, _cr_ap_m, (_cc_mode)_cr_m, _cr_arch_perm);                                       \
     }
